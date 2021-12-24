91400d7f5a05c9837ec6ea360388fa16651fb109
Ed Bieda <edward.bieda@gmail.com>
2021-12-24 15:43:42 +0000

    removing unrequired API files

---
+++ src/pages/api/people/about.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+type Data = {
+  name: string;
+  age: string;
+};
+
+const prisma = new PrismaClient();
+
+export default async function aboutHandler(req: NextApiRequest, res: NextApiResponse<Data|Data[]|null>) {
+  const person = await prisma.person.findMany({
+    where: {
+      name: 'Ed'
+    }
+  })
+
+  res.status(200).json(person);
+}
